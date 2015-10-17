c42a274ec6812c545a3f57106eecfa2fcd8dc30f
John Coates <acapulco1988@gmail.com>
2015-10-17 13:40:50 -0700

    added message if CSSketch wasn't able to load.

--- CSSketch.sketchplugin/Contents/Sketch/src/CSSketch.js
+++ CSSketch.sketchplugin/Contents/Sketch/src/CSSketch.js
@@ -2,33 +2,63 @@ var CSSketchContext = null;
 
 var layoutLayers = function(context) {
   CSSketchContext = context;
-  loadCSSketchAsNeeded();
 
-  var mainController = CSKMainController.sharedInstance();
-  mainController.layoutLayersWithContext(context);
+  if (loadCSSketchAsNeeded()) {
+    var mainController = CSKMainController.sharedInstance();
+    mainController.layoutLayersWithContext(context);
+  }
 }
 
 var setPageStylesheet = function(context) {
   CSSketchContext = context;
-  loadCSSketchAsNeeded();
 
-  var mainController = CSKMainController.sharedInstance();
-  mainController.selectStylesheetWithContext(context);
+  if (loadCSSketchAsNeeded()) {
+    var mainController = CSKMainController.sharedInstance();
+    mainController.selectStylesheetWithContext(context);
+  }
 }
 
 function loadCSSketchAsNeeded() {
   if (!NSClassFromString("CSKFileMonitor")){
-    loadBundle("CSSketch Helper.bundle");
+    if (!loadBundle("CSSketch Helper.bundle")) {
+      log("Couldn't load bundle!");
+      return false;
+    }
   }
+
+  return true;
 }
 
 function loadBundle(filename) {
   var pluginURL = pluginPathURL();
-	var bundleURL = pluginURL.URLByAppendingPathComponent(filename);
+  var bundleURL = pluginURL.URLByAppendingPathComponent(filename);
   var bundle = [NSBundle bundleWithURL: bundleURL];
-  [bundle load];
+	if (!bundle) {
+		showNotification("Bundle missing!");
+		return false;
+	}
+
+	var loaded = [bundle load];
+	if (!loaded) {
+		showNotification("Couldn't load CSSketch bundle! Try allowing apps downloaded from anywhere (System Preferences -> Security & Privacy)");
+    return false;
+	}
+
+	return loaded;
 }
 
 function pluginPathURL() {
   return CSSketchContext.plugin.url();
+}
+
+function showNotification(message) {
+  // NSUserNotification only shows if app is active
+  var notification =  [[NSUserNotification alloc] init];
+  notification.title = @"CSSketch";
+  notification.informativeText = message;
+  notification.soundName = NSUserNotificationDefaultSoundName;
+  [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+
+  // displayMessage only shows if app is active
+  CSSketchContext.document.displayMessage("CSSketch: "+ message);
 }
