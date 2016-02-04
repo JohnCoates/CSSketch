7464369fefe1502b41bed803e65203eacc9895db
John Coates <acapulco1988@gmail.com>
2016-02-04 15:36:35 -0800

    added setupSymlinks.rb

---
+++ setupSymlinks.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+# Setup Symlinks for easy development debugging
+require 'fileutils'
+require 'open3'
+
+projectDirectory = File.expand_path(File.dirname(__FILE__))
+plugin = projectDirectory + "/CSSketch.sketchplugin"
+frameworkPlugin = projectDirectory + "/External/SketchKit/SketchKit.sketchplugin"
+
+pluginsFolder = File.expand_path("~/Library/Application Support/com.bohemiancoding.sketch3/Plugins")
+
+pluginDestination = pluginsFolder + "/CSSketch.sketchplugin"
+if File.exists?(pluginDestination) == true
+	FileUtils.rm_r(pluginDestination)
+end
+
+if File.symlink(plugin, pluginDestination)  == false
+	puts "Failed to make plugin symlink!"
+	exit 1
+end
+
+puts "Made plugin symlink."
+
+frameworkDestination = pluginsFolder + "/SketchKit.sketchplugin"
+if File.exists?(frameworkDestination) == true
+	FileUtils.rm_r(frameworkDestination)
+end
+
+if File.symlink(frameworkPlugin, frameworkDestination)  == false
+	puts "Failed to make framework symlink!"
+	exit 1
+end
+
+puts "Made framework symlink."
